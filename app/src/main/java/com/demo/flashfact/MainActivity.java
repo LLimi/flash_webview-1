92c02408963d2625a914d042b1af4a137baa9b44
Yahyr Paredes <yahyrparedesarteaga@gmail.com>
2021-12-09 00:12:56 -0500

    initial commit

---
+++ app/src/main/java/com/demo/flashfact/MainActivity.java
@@ -0,0 +1,66 @@
+package com.demo.flashfact;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.http.SslError;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+
+
+        myWebView.setWebChromeClient(new WebChromeClient() {
+            public void onProgressChanged(WebView view, int progress) {
+
+                Log.e(TAG, "Loading...");
+                Log.e(TAG, " : " + (progress * 100));
+
+                if (progress == 100)
+                    Log.e(TAG, "LOADING COMPLETE");
+            }
+        });
+
+        myWebView.setWebViewClient(new WebViewClient() {
+
+            @Override
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                Log.d("Failure Url :" , failingUrl);
+            }
+
+            @Override
+            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+                Log.d("Ssl Error:",handler.toString() + "error:" +  error);
+                handler.proceed();
+            }
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+        myWebView.getSettings().setDomStorageEnabled(true);
+        myWebView.getSettings().setJavaScriptEnabled(true);
+        myWebView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+
+        myWebView.getSettings().setLoadWithOverviewMode(true);
+        myWebView.getSettings().setUseWideViewPort(true);
+        myWebView.loadUrl("https://flash.visafacturacion.com/ecommerce");
+
+    }
+}
